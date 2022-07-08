e1d72af111e7a5d1d530d4ead3ccc7f182c5edaf
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2022-07-08 11:03:10 -0300

    Update

--- routes/activity.js
+++ routes/activity.js
@@ -2,11 +2,6 @@
 const util = require('util');
 const axios = require("axios");
 
-const JWT = (body, secret, cb) => {
-	if (!body) return cb(new Error('invalid jwtdata'));
-	require('jsonwebtoken').verify(body.toString('utf8'), secret, { algorithm: 'HS256' }, cb);
-};
-
 exports.logExecuteData = [];
 const logData = (req) => { // Log data from the request and put it in an array accessible to the main app.
     exports.logExecuteData.push({
@@ -47,29 +42,14 @@ const logData = (req) => { // Log data from the request and put it in an array a
     console.log("originalUrl: " + req.originalUrl);
 }
 
-/*
- * POST Handler for / route of Activity (this is the edit route).
- */
-exports.edit = (req, res) => {
-    logData(req);
-    res.send(200, 'Edit');
+const JWT = (body, secret, cb) => {
+	if (!body) return cb(new Error('invalid jwtdata'));
+	require('jsonwebtoken').verify(body.toString('utf8'), secret, { algorithm: 'HS256' }, cb);
 };
 
-/*
- * POST Handler for /save/ route of Activity.
- */
-exports.save = (req, res) => {
-    logData(req);
-    res.send(200, 'Save');
-};
-
-/*
- * POST Handler for /execute/ route of Activity.
- */
 exports.execute = function (req, res) {
     console.log(JSON.stringify(req.headers));
     JWT(req.body, process.env.jwtSecret, async (err, decoded) => {
-        // verification error -> unauthorized request
         if (err) {
             console.error(err);
             return res.status(401).end();
@@ -111,15 +91,13 @@ exports.execute = function (req, res) {
                 .catch((error) => {
                     console.log('Error:');
                     console.log(error);
-                    console.log(JSON.stringify(error));
                     packsValidationFailed = true;
                     packsValidationError = JSON.stringify(error);
                 });
 
             res.send(200, {
                 phoneNumberCanBuyAPack: packsValidationFailed ? false : ((packsValidationResponse.canBePurchased === true && packsValidationResponse.packId) ? true : false),
-                packsValidationFailed,
-                packsValidationError 
+                packsValidationFailed
             });
         } else {
             console.error('inArguments invalid.');
@@ -128,46 +106,27 @@ exports.execute = function (req, res) {
     });
 };
 
-/*
- * POST Handler for /publish/ route of Activity.
- */
+exports.edit = (req, res) => {
+    logData(req);
+    res.send(200, 'Edit');
+};
+
+exports.save = (req, res) => {
+    logData(req);
+    res.send(200, 'Save');
+};
+
 exports.publish = (req, res) => {
     logData(req);
     res.send(200, 'Publish');
 };
 
-/*
- * POST Handler for /validate/ route of Activity.
- */
 exports.validate = (req, res) => {
     logData(req);
     res.send(200, 'Validate');
 };
 
-/*
- * POST Handler for /Stop/ route of Activity.
- */
 exports.stop = (req, res) => {
     logData(req);
     res.send(200, 'Stop');
-};
-
-/**
- * This function relies on the environment variables being set.
- * 
- * This function invokes the enhanced package authentication.
- * This would return an access token that can be used to call additional Marketing Cloud APIs.
- * 
- */
-const retrieveToken = () => {
-    axios.post(
-        `${process.env.authenticationUrl}/v2/token`,
-        {
-            grant_type: 'client_credentials',
-            client_id: process.env.clientId,
-            client_secret: process.env.clientSecret
-        }
-    )
-        .then(response => response.data['access_token'])
-        .catch(error => error);
 };
