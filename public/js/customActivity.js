49c3a4a5bfb07b0655c075c85918d9ce874b5f08
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2022-07-08 11:12:46 -0300

    .

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -5,16 +5,13 @@ define(['postmonger'], (Postmonger) => {
     let payload = {};
     let eventDefinitionKey;
 
-    $(window).ready(onRender);
-    connection.on('initActivity', initialize);
-    connection.on('clickedNext', save); // Save function within MC
-
-    function onRender() {
-        // JB will respond the first time 'ready' is called with 'initActivity'
+    $(window).ready(() => {
         connection.trigger('ready');
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
-    }
+    });
+    connection.on('initActivity', initialize);
+    connection.on('clickedNext', save);
 
     /**
      * This function is to pull out the event definition within journey builder.
