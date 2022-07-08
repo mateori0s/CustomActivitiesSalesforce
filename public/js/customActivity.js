f7110e658f0e61d2b09f1b5cac5d7fe8faa070bc
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2022-07-08 11:22:33 -0300

    .

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -10,34 +10,33 @@ define(['postmonger'], (Postmonger) => {
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
     });
-    connection.on('initActivity', initialize);
-    connection.on('clickedNext', save);
 
-    /**
-     * This function is to pull out the event definition within journey builder.
-     * With the eventDefinitionKey, you are able to pull out values that passes through the journey
-     */
-    connection.trigger('requestTriggerEventDefinition');
-    connection.on('requestedTriggerEventDefinition', function (eventDefinitionModel) {
-        if (eventDefinitionModel) {
-            eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
-            // console.log('Request Trigger >>>', JSON.stringify(eventDefinitionModel));
-        }
+    connection.on('initActivity', (data) => {
+        if (data) payload = data;
+
+        const inArguments = Boolean(
+            data.arguments &&
+            data.arguments.execute &&
+            data.arguments.execute.inArguments &&
+            data.arguments.execute.inArguments.length > 0
+        ) ? data.arguments.execute.inArguments : [];
+    
+        const messageTextArg = inArguments.find(arg => arg.messageText);
+        if (messageTextArg) document.getElementById('messageText').value = messageTextArg.messageText;
+
+        const subjectArg = inArguments.find(arg => arg.subject);
+        if (subjectArg) document.getElementById('subject').value = subjectArg.subject;
+
+        const urgenteArg = inArguments.find(arg => arg.urgente);
+        let urgenteIdSuffix = (urgenteArg && urgenteArg.urgente === true) ? 'true' : 'false';
+        document.getElementById(`urgente-${urgenteIdSuffix}`).checked = true;
+
+        const validarArg = inArguments.find(arg => arg.validar);
+        let validarIdSuffix = (validarArg && validarArg.urgente === true) ? 'true' : 'false';
+        document.getElementById(`validar-${validarIdSuffix}`).checked = true;
     });
 
-    function initialize(data) {
-        if (data) {
-            payload = data;
-        }
-        initialLoad(data);
-        // parseEventSchema();
-    }
-
-    /**
-     * Save function is fired off upon clicking of "Done" in Marketing Cloud
-     * The config.json will be updated here if there are any updates to be done via Front End UI
-     */
-    function save() {
+    connection.on('clickedNext', () => {
         let urgente = null;
         if (document.getElementById('urgente-true').checked) urgente = true;
         if (document.getElementById('urgente-false').checked) urgente = false;
@@ -61,37 +60,10 @@ define(['postmonger'], (Postmonger) => {
         ];
         payload['metaData'].isConfigured = true;
         connection.trigger('updateActivity', payload);
-    }
+    });
 
-    /**
-     * 
-     * @param {*} data
-     * 
-     * This data param is the config json payload that needs to be loaded back into the UI upon opening the custom application within journey builder 
-     * This function is invoked when the user clicks on the custom activity in Marketing Cloud. 
-     * If there are information present, it should be loaded back into the appropriate places. 
-     * e.g. input fields, select lists
-     */
-    function initialLoad(data) {
-        const inArguments = Boolean(
-            data.arguments &&
-            data.arguments.execute &&
-            data.arguments.execute.inArguments &&
-            data.arguments.execute.inArguments.length > 0
-        ) ? data.arguments.execute.inArguments : [];
-        
-        const messageTextArg = inArguments.find(arg => arg.messageText);
-        if (messageTextArg) document.getElementById('messageText').value = messageTextArg.messageText;
-
-        const subjectArg = inArguments.find(arg => arg.subject);
-        if (subjectArg) document.getElementById('subject').value = subjectArg.subject;
-
-        const urgenteArg = inArguments.find(arg => arg.urgente);
-        let urgenteIdSuffix = (urgenteArg && urgenteArg.urgente === true) ? 'true' : 'false';
-        document.getElementById(`urgente-${urgenteIdSuffix}`).checked = true;
-
-        const validarArg = inArguments.find(arg => arg.validar);
-        let validarIdSuffix = (validarArg && validarArg.urgente === true) ? 'true' : 'false';
-        document.getElementById(`validar-${validarIdSuffix}`).checked = true;
-    };
+    connection.trigger('requestTriggerEventDefinition');
+    connection.on('requestedTriggerEventDefinition', (eventDefinitionModel) => {
+        if (eventDefinitionModel) eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
+    });
 });
